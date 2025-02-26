7117b9ccb16df3f7cffb619042382e90133a5b5f
mhaouas <mat.haouas@gmail.com>
2025-02-26 13:50:55 +0100

    Starting project

---
+++ Py00/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_world = ["Hello", "World!"]
+ft_country = ("Hello", "France!")
+ft_city = {"Hello", "Angoulême!"}
+ft_campus = {"Hello" : "42 Angoulême!"}
+
+print(ft_world)
+print(ft_country)
+print(ft_city)
+print(ft_campus)
